17c3c4f33f4bd27e70dfa567120e0bd4b323716c
Stephane <ssantos@norsys.fr>
2020-01-22 12:07:20 +0100

    add deploy stage

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
 
+echo $PACKAGE_VERSION
 
-zip -r $zipFile ./* >> /dev/null
+if [ -d "epitech/t-web-700/api" ]; then
+  rm -R "epitech/t-web-700/api"
+fi
 
-$publishCommand2
+if [ -d "epitech/t-web-700/api" ]; then
+    echo "Le workspace epitech/t-web-700/api existe deja."
+else
+    mkdir -p "epitech/t-web-700/api"
+fi
+
+wget -P "epitech/download" "http://185.216.25.54:32778/repository/node-app/	t-dev-700-api/t-dev-700-api-$PACKAGE_VERSION.tgz"
+
+tar -zxvf epitech/download/t-dev-700-api-$PACKAGE_VERSION.tgz -C epitech/t-web-700/api/
+
