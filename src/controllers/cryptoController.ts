953dda9a304770ca431163044bad041488c33e59
Stephane <ssantos@norsys.fr>
2020-01-27 14:32:49 +0100

    Add put and get crypto endPoint

--- src/controllers/cryptoController.ts
+++ src/controllers/cryptoController.ts
@@ -4,37 +4,22 @@ import {CryptoService} from "../services/crypto.service";
 import * as HttpStatus from "http-status-codes";
 import {ApiResponseError} from "../resources/interfaces/ApiResponseError";
 import {Crypto} from "../entities/Crypto";
+import {UserService} from "../services/users.service";
+import errors from "../assets/i18n/en/errors";
 
 
 const cryptoRouter: Router = Router();
 
 cryptoRouter.route('/')
-
     .get(async (req: Request, res: Response, next: NextFunction) => {
-
-        const baseUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
-        var options = {
-            uri: baseUrl,
-            qs: {
-                'start': '1',
-                'limit': '5000',
-                'convert': 'USD'
-            },
-            headers: {
-                'X-CMC_PRO_API_KEY': '8eaa69e9-ac42-4595-884a-6f32b47968d3'
-            },
-            json: true,
-            gzip: true
-        };
-
-        const result = await request.get(options);
+        const cryptoService = new CryptoService();
 
         try {
-            const result = await request.get(options);
+            const response = await cryptoService.getAll();
             // return 200 even if no user found. Empty array. Not an error
             res.status(HttpStatus.OK).json({
                 success: true,
-                data: result
+                data: response
             });
         } catch (err) {
             const error: ApiResponseError = {
@@ -94,32 +79,25 @@ cryptoRouter.route('/sparkline/:symbol')
 cryptoRouter.route('/:symbol')
 
     .get(async (req: Request, res: Response, next: NextFunction) => {
-
         const cryptoID = req.params.symbol;
-        const baseUrl = 'https://api.nomics.com/v1/currencies/ticker';
-        var options = {
-            uri: baseUrl,
-            qs: {
-                'ids': cryptoID,
-                'convert': "EUR",
-                'key': 'af6b695c10a1e33cd0a11198a09b5534',
-            },
-            headers: {
-                'key': 'af6b695c10a1e33cd0a11198a09b5534'
-            },
-            json: true,
-            gzip: true
-        };
-
-        const result = await request.get(options);
-
+        const cryptoService = new CryptoService();
         try {
-            const result = await request.get(options);
-            // return 200 even if no user found. Empty array. Not an error
+            const crypto = await cryptoService.getById(cryptoID);
+
+            // if user not found
+            if (!crypto) {
+                res.status(HttpStatus.NOT_FOUND).json({
+                    success: false,
+                    message: `${errors.entityNotFound}: crypto id`
+                });
+                return;
+            }
+            // return found user
             res.status(HttpStatus.OK).json({
                 success: true,
-                data: result
+                user: crypto
             });
+
         } catch (err) {
             const error: ApiResponseError = {
                 code: HttpStatus.BAD_REQUEST,
@@ -162,6 +140,50 @@ cryptoRouter.route('/:symbol')
         try {
             const response = await cryptoService.insert(crypto);
             // return 200 even if no user found. Empty array. Not an error
+            res.status(HttpStatus.OK).json({
+                success: true,
+                data: response
+            });
+        } catch (err) {
+            const error: ApiResponseError = {
+                code: HttpStatus.BAD_REQUEST,
+                errorObj: err
+            };
+            next(error);
+        }
+    })
+    .put(async (req: Request, res: Response, next: NextFunction) => {
+        console.log("PUT");
+        const cryptoService = new CryptoService();
+        const cryptoID = req.params.symbol;
+        const baseUrl = 'https://api.nomics.com/v1/currencies/ticker';
+        var options = {
+            uri: baseUrl,
+            qs: {
+                'ids': cryptoID,
+                'convert': "EUR",
+                'key': 'af6b695c10a1e33cd0a11198a09b5534',
+            },
+            headers: {
+                'key': 'af6b695c10a1e33cd0a11198a09b5534'
+            },
+            json: true,
+            gzip: true
+        };
+
+        const result = await request.get(options);
+        const crypto = await cryptoService.getByIds(cryptoID);
+        for (let data of result){
+            crypto.IDs = data.id;
+            crypto.name = data.name;
+            crypto.URL = data.logo_url;
+            crypto.currentPrice = data.price;
+            crypto.highPrice = data.high;
+        }
+        console.log(crypto);
+        try {
+            const response = await cryptoService.update(crypto);
+            // return 200 even if no crypto found. Empty array. Not an error
             res.status(HttpStatus.OK).json({
                 success: true,
                 data: response
